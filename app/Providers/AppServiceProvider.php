8030053a17b5b74accc3cc3337cf48291d6d5057
Al Mutawakkil <almutawakkilpaletari@gmail.com>
2025-09-15 18:54:33 +0800

    Tambah build 0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// Tambahkan baris ini
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan blok if ini
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
