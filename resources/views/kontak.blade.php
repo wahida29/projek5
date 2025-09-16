d71395d1baece5e070a19cd5d98c7dd628ca6afa
Al Mutawakkil <almutawakkilpaletari@gmail.com>
2025-09-17 02:56:59 +0800

    view9

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -33,14 +33,14 @@
             <h3 class="mb-6 text-4xl font-extrabold text-gray-800">Lokasi Kami</h3>
             <div class="relative w-full overflow-hidden rounded-lg shadow-lg h-96">
                 <!-- Embed Google Maps -->
-                <<iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63576.482771549825!2d119.39146467910159!3d-5.178986399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee3bf2baf0669%3A0x334795a8585bb979!2sKrusit%20Makassar!5e0!3m2!1sid!2sid!4v1758048648962!5m2!1sid!2sid"
-                width="600"
-                height="450"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63576.482771549825!2d119.39146467910159!3d-5.178986399999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee3bf2baf0669%3A0x334795a8585bb979!2sKrusit%20Makassar!5e0!3m2!1sid!2sid!4v1758048648962!5m2!1sid!2sid"
+                    width="100%"
+                    height="100%"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                ></iframe>
             </div>
         </div>
     </section>
