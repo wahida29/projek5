7c9dfb6291efb7ac2dbc92da88b1bf58e26c85f0
Al Mutawakkil <almutawakkilpaletari@gmail.com>
2025-09-16 12:24:36 +0800

    tampilan0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,14 +45,6 @@
                 </a>
             </div>
         </div>
-
-        <div class="absolute flex flex-col items-center gap-2 text-sm text-gray-400 -translate-x-1/2 bottom-8 left-1/2 animate-bounce">
-            <span>Lihat Menu</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-            </svg>
-        </div>
-
     </main>
 </body>
 </html>
