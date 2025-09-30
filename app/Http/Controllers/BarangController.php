9b5074b0bc2691839fc3b079239bbba8ee5f6ee6
Wahida <wahidajarrakwaida.com>
2025-09-30 11:29:47 +0800

    upload logo 5

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -143,11 +143,7 @@ public function storeKopi(Request $request)
     'name' => 'required|string|max:255',
     'description' => 'nullable|string',
     'price' => 'required|numeric',
-    'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
 ]);
-if ($request->hasFile('image')) {
-    $path = $request->file('image')->store('images', 'public');
-}
 
 
     $barang = Menu::create([
@@ -155,7 +151,6 @@ if ($request->hasFile('image')) {
         'description' => $request->description,
         'category' => 'kopi',
         'price' => $request->price,
-        'image' => $request->image,
     ]);
 
     return response()->json([
