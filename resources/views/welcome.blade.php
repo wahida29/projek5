30f70205fe682585e84c0eb8ea0093756d43e4fe
Wahida <wahidajarrakwaida.com>
2025-09-30 10:06:59 +0800

    upload logo 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="home.css">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
